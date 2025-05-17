3e7ba1a1d0c5490ee98b7e7b1192e487f0a8e63b
Drygo <davidgp2b@outlook.com>
2025-05-17 14:09:47 -0700

    💣 Added Bomb Steal listener, Remove Bomb to Offline Players listener.

---
+++ src/main/java/org/eldrygo/GoldenBomb/Game/Listener/PlayerListener.java
@@ -0,0 +1,50 @@
+package org.eldrygo.GoldenBomb.Game.Listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.eldrygo.GoldenBomb.Game.Managers.BombManager;
+import org.eldrygo.GoldenBomb.Game.Managers.GameManager;
+import org.eldrygo.GoldenBomb.GoldenBomb;
+
+public class PlayerListener implements Listener {
+
+    private final GameManager gameManager;
+    private final BombManager bombManager;
+    private final GoldenBomb plugin;
+
+    public PlayerListener(GameManager gameManager, BombManager bombManager, GoldenBomb plugin) {
+        this.gameManager = gameManager;
+        this.bombManager = bombManager;
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerJoin (PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (gameManager.getCurrentState() != GameManager.GameState.STOPPED) return;
+        if (!bombManager.getBombTeam().getEntries().contains(String.valueOf(player.getUniqueId()))) return;
+
+        if (!bombManager.getPlayersWithBomb().contains(player)) return;
+
+        bombManager.takeBomb(player);
+    }
+
+    @EventHandler
+    public void stealBomb (EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof Player victim)) return;
+        if (!(event.getDamager() instanceof Player damager)) return;
+
+        if (gameManager.getCurrentState() != GameManager.GameState.RUNNING) return;
+
+        if (!bombManager.getPlayersWithBomb().contains(victim)) return;
+        if (bombManager.getPlayersWithBomb().contains(damager)) return;
+
+        bombManager.takeBomb(victim);
+        bombManager.addBomb(damager);
+
+        plugin.getLogger().warning("El jugador " + damager.getName() + " robó la Golden Bomb de " + victim.getName() + "!");
+    }
+}
