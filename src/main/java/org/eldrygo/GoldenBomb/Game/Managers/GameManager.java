4d3c2246a5c77202856555ce89bdfecd39542ad9
Drygo <davidgp2b@outlook.com>
2025-05-17 15:05:46 -0700

    💣 Added Game Runnable.

--- src/main/java/org/eldrygo/GoldenBomb/Game/Managers/GameManager.java
+++ src/main/java/org/eldrygo/GoldenBomb/Game/Managers/GameManager.java
@@ -1,6 +1,14 @@
 package org.eldrygo.GoldenBomb.Game.Managers;
 
 public class GameManager {
+    private final BombManager bombManager;
+    private final RunnableManager runnableManager;
+
+    public GameManager(BombManager bombManager, RunnableManager runnableManager) {
+        this.bombManager = bombManager;
+        this.runnableManager = runnableManager;
+    }
+
     public enum GameState {
         RUNNING,
         STOPPED
@@ -10,13 +18,20 @@ public class GameManager {
 
     public void startGame() {
         currentState = GameState.RUNNING;
+        runnableManager.startTask();
     }
 
     public void stopGame() {
         currentState = GameState.STOPPED;
+        runnableManager.stopTask();
     }
 
     public GameState getCurrentState() {
         return currentState;
+    }
+
+    public void resetAll() {
+        stopGame();
+        bombManager.resetBombs();
     }
 }
---
+++ src/main/java/org/eldrygo/GoldenBomb/Game/Managers/RunnableManager.java
@@ -0,0 +1,65 @@
+package org.eldrygo.GoldenBomb.Game.Managers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.eldrygo.GoldenBomb.GoldenBomb;
+
+public class RunnableManager {
+    private BukkitRunnable task;
+    private final GoldenBomb plugin;
+    private final BombManager bombsManager;
+
+    public RunnableManager(GoldenBomb plugin, BombManager bombsManager) {
+        this.plugin = plugin;
+        this.bombsManager = bombsManager;
+    }
+
+    public void startTask() {
+        plugin.getLogger().info("GoldenBomb runnable started.");
+        task = new BukkitRunnable() {
+            @Override
+            public void run() {
+                execute();
+            }
+        };
+        task.runTaskTimer(plugin, 0L, 20L);
+    }
+
+    public void stopTask() {
+        if (task != null) {
+            task.cancel();
+            plugin.getLogger().info("GoldenBomb runnable stopped.");
+        }
+    }
+
+    public boolean taskExists() {
+        return task != null;
+    }
+
+    private void execute() {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (bombsManager.getPlayersWithBomb().contains(p)) {
+                if (!p.isGlowing()) {
+                    p.setGlowing(true);
+                }
+
+                if (!bombsManager.getBombTeam().getEntries().contains(p.getName())) {
+                    bombsManager.getBombTeam().addEntity(p);
+                }
+
+                // COMPROBACION DEL ITEM DE LA BOMBA.
+            } else {
+                if (p.isGlowing() && !p.isOp()) {
+                    p.setGlowing(false);
+                }
+
+                if (bombsManager.getBombTeam().getEntries().contains(p.getName())) {
+                    bombsManager.getBombTeam().removeEntity(p);
+                }
+
+                // COMPROBACION DEL ITEM DE LA BOMBA.
+            }
+        }
+    }
+}
