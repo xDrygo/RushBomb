bcefe5e533731a2cc7deeefb4c89a8ac421d4941
Drygo <davidgp2b@outlook.com>
2025-05-17 15:07:23 -0700

    💣 Added instances and stop game runnable on onDisable()

--- src/main/java/org/eldrygo/GoldenBomb/GoldenBomb.java
+++ src/main/java/org/eldrygo/GoldenBomb/GoldenBomb.java
@@ -3,29 +3,32 @@ package org.eldrygo.GoldenBomb;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.eldrygo.GoldenBomb.Game.Managers.BombManager;
 import org.eldrygo.GoldenBomb.Game.Managers.GameManager;
+import org.eldrygo.GoldenBomb.Game.Managers.RunnableManager;
 import org.eldrygo.GoldenBomb.Managers.ConfigManager;
 import org.eldrygo.GoldenBomb.Utils.ChatUtils;
 import org.eldrygo.GoldenBomb.Utils.LoadUtils;
 import org.eldrygo.GoldenBomb.Utils.LogsUtils;
+import org.eldrygo.GoldenBomb.Utils.OtherUtils;
 
 public class GoldenBomb extends JavaPlugin {
     public String prefix;
     public String version;
     private LogsUtils logsUtils;
     private GameManager gameManager;
+    private RunnableManager runnableManager;
 
     @Override
     public void onEnable() {
         version = getDescription().getVersion();
 
         this.logsUtils = new LogsUtils(this);
-        this.gameManager = new GameManager();
-
+        BombManager bombManager = new BombManager(this);
+        this.runnableManager = new RunnableManager(this, bombManager);
+        this.gameManager = new GameManager(bombManager, runnableManager);
         ConfigManager configManager = new ConfigManager(this);
         ChatUtils chatUtils = new ChatUtils(configManager, this);
-        BombManager bombManager = new BombManager(this);
         LoadUtils loadUtils = new LoadUtils(this, chatUtils, configManager, gameManager, bombManager);
-
+        OtherUtils.setPlugin(this);
         loadUtils.loadFeatures();
 
         logsUtils.sendStartupMessage();
@@ -34,6 +37,7 @@ public class GoldenBomb extends JavaPlugin {
     @Override
     public void onDisable() {
         if (gameManager.getCurrentState() == GameManager.GameState.STOPPED) gameManager.stopGame();
+        runnableManager.stopTask();
 
         logsUtils.sendShutdownMessage();
     }
