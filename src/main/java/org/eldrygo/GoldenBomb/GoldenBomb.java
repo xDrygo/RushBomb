f8479177c4928941f97bbe0ea11f6a81fdeb0c50
Drygo <davidgp2b@outlook.com>
2025-05-17 23:05:31 -0700

    💣 Added some instance initialization.

--- src/main/java/org/eldrygo/GoldenBomb/GoldenBomb.java
+++ src/main/java/org/eldrygo/GoldenBomb/GoldenBomb.java
@@ -3,12 +3,14 @@ package org.eldrygo.GoldenBomb;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.eldrygo.GoldenBomb.Game.Managers.BombManager;
 import org.eldrygo.GoldenBomb.Game.Managers.GameManager;
+import org.eldrygo.GoldenBomb.Game.Managers.ItemManager;
 import org.eldrygo.GoldenBomb.Game.Managers.RunnableManager;
-import org.eldrygo.GoldenBomb.Managers.ConfigManager;
-import org.eldrygo.GoldenBomb.Utils.ChatUtils;
-import org.eldrygo.GoldenBomb.Utils.LoadUtils;
-import org.eldrygo.GoldenBomb.Utils.LogsUtils;
-import org.eldrygo.GoldenBomb.Utils.OtherUtils;
+import org.eldrygo.GoldenBomb.Lib.Time.Managers.TimeManager;
+import org.eldrygo.GoldenBomb.Plugin.Managers.ConfigManager;
+import org.eldrygo.GoldenBomb.Plugin.Utils.ChatUtils;
+import org.eldrygo.GoldenBomb.Plugin.Utils.LoadUtils;
+import org.eldrygo.GoldenBomb.Plugin.Utils.LogsUtils;
+import org.eldrygo.GoldenBomb.Plugin.Utils.OtherUtils;
 
 public class GoldenBomb extends JavaPlugin {
     public String prefix;
@@ -22,12 +24,14 @@ public class GoldenBomb extends JavaPlugin {
         version = getDescription().getVersion();
 
         this.logsUtils = new LogsUtils(this);
-        BombManager bombManager = new BombManager(this);
+        ItemManager itemManager = new ItemManager(this);
+        BombManager bombManager = new BombManager(this, itemManager);
         this.runnableManager = new RunnableManager(this, bombManager);
-        this.gameManager = new GameManager(bombManager, runnableManager);
         ConfigManager configManager = new ConfigManager(this);
+        TimeManager timeManager = new TimeManager();
+        this.gameManager = new GameManager(bombManager, runnableManager, timeManager, this, configManager);
         ChatUtils chatUtils = new ChatUtils(configManager, this);
-        LoadUtils loadUtils = new LoadUtils(this, chatUtils, configManager, gameManager, bombManager);
+        LoadUtils loadUtils = new LoadUtils(this, chatUtils, configManager, gameManager, bombManager, timeManager);
         OtherUtils.setPlugin(this);
         loadUtils.loadFeatures();
 
