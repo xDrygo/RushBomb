fd399b9afec8feb70b4b3b4dfe222d2c1d0e75f1
Drygo <davidgp2b@outlook.com>
2025-05-18 11:37:18 -0700

    🍕 Proyect Set-Up

---
+++ src/main/java/org/eldrygo/PacTNTMan/PacTNTMan.java
@@ -0,0 +1,15 @@
+package org.eldrygo.PacTNTMan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PacTNTMan extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
