0cceec66815b9a1d210d37c6a2f206b529810c89
Drygo <davidgp2b@outlook.com>
2025-05-19 17:38:40 -0700

    🔧 Fix: Changed team.addEntity() to team.addEntry() - Version 0.0.04

--- src/main/java/org/eldrygo/KothTNT/Game/Managers/BombManager.java
+++ src/main/java/org/eldrygo/KothTNT/Game/Managers/BombManager.java
@@ -27,7 +27,7 @@ public class BombManager {
     public void addBomb(Player player) {
         if (!playersWithBomb.contains(player)) {
             playersWithBomb.add(player);
-            bombTeam.addEntity(player);
+            bombTeam.addEntry(player.getName());
             player.setGlowing(true);
             itemManager.setBombHead(player);
         }
@@ -36,7 +36,7 @@ public class BombManager {
     public void takeBomb(Player player) {
         if (playersWithBomb.contains(player)) {
             playersWithBomb.remove(player);
-            bombTeam.removeEntity(player);
+            bombTeam.removeEntry(player.getName());
             player.setGlowing(false);
             itemManager.removeBombHead(player);
         }
